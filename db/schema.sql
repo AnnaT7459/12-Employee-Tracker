1929f6a5f72d8a013f6216bc3e7f197636537405
AnnaT7459 <141446349+AnnaT7459@users.noreply.github.com>
2023-12-10 16:26:35 -0600

    code added to create departments, roles, and employees tables in schema

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS business_db;
+CREATE DATABASE business_db;
+
+USE business_db;
+
+-- Department table
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) 
+);
+
+-- Role table
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT NOT NULL, 
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+-- Employee table
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL, 
+    FOREIGN KEY (role_id) 
+    REFERENCES roles(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) 
+    REFERENCES employee(id)
+);
